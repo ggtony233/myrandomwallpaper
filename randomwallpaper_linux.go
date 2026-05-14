c0d37d7eeb680a8fe30e023774ed080bb4446115
1923113775@qq.com <1923113775@qq.com>
2026-05-14 23:41:47 +0800

    add only wallpaper_path out by '-t'

--- randomwallpaper_linux.go
+++ randomwallpaper_linux.go
@@ -4,12 +4,21 @@
 package main
 
 import (
+	"flag"
+	"os"
+
 	"github.com/ggtony233/random_image/utils"
 	"github.com/reujab/wallpaper"
 )
 
 func main() {
+	Flag := flag.Bool("t", false, "only filepath")
+	flag.Parse()
 	path := utils.RandomImagePath(utils.GetJsonPath())
+	if *Flag {
+		os.Stdout.WriteString(path)
+		os.Exit(0)
+	}
 	err := wallpaper.SetFromFile(path)
 	if err != nil {
 		print(err.Error())
